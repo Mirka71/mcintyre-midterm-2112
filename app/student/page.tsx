c5a04a25afa1fd28ab4a15d6aacf4497fff9b8d0
Mirka McIntyre <mirka.kiana@gmail.com>
2025-10-27 09:29:46 -0400

    5. Made student folder with page with student info

---
+++ app/student/page.tsx
@@ -0,0 +1,11 @@
+// 5. Made student folder with page with student info
+export default function Student() {
+    return(
+    <article>
+        <h1>Student Info</h1>
+            <p>Mirka McIntyre</p>
+            <p>SN: 200627729</p>
+    </article>
+    );
+}
+        
